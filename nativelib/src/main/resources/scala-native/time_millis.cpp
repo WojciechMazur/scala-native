33c8e0ac77de5620f4a067cbe3c89c1a12e93d3e
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-03-19 21:54:11 +0100

    Implement scalanative_current_time_millis for windows

--- nativelib/src/main/resources/scala-native/time_millis.cpp
+++ nativelib/src/main/resources/scala-native/time_millis.cpp
@@ -1,19 +1,31 @@
+#if defined(_WIN32)
+#include <windows.h>
+#else
 #include <stdio.h>
 #include <sys/time.h>
+#endif
 
+extern "C" {
 long long scalanative_current_time_millis() {
     long long current_time_millis;
 
 #define MILLIS_PER_SEC 1000LL
 #define MICROS_PER_MILLI 1000LL
 
+#if defined(_WIN32)
+    SYSTEMTIME time;
+    GetSystemTime(&time);
+    current_time_millis = (time.wSecond * MILLIS_PER_SEC) + time.wMilliseconds;
+#else
     struct timeval tv;
     gettimeofday(&tv, NULL);
     current_time_millis =
         tv.tv_sec * MILLIS_PER_SEC + tv.tv_usec / MICROS_PER_MILLI;
+#endif
 
 #undef MILLIS_PER_SEC
 #undef MICROS_PER_MILLI
 
     return current_time_millis;
+}
 }

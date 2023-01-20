9623d24e8d3da5098ef61ba508a93fefebeec46f
Wojciech Mazur <wmazur@virtuslab.com>
2023-01-20 01:12:46 +0100

    Fix STW threads synchronization on Windows. Remove issue with deadlocks

--- nativelib/src/main/resources/scala-native/gc/immix/Synchronizer.c
+++ nativelib/src/main/resources/scala-native/gc/immix/Synchronizer.c
@@ -28,7 +28,6 @@ extern safepoint_t *scalanative_gc_safepoint;
 
 #ifdef _WIN32
 static LPTOP_LEVEL_EXCEPTION_FILTER defaultFilter;
-thread_local static HANDLE wakeupEvent;
 static long SafepointTrapHandler(EXCEPTION_POINTERS *ex) {
     switch (ex->ExceptionRecord->ExceptionCode) {
     case EXCEPTION_ACCESS_VIOLATION:
@@ -94,9 +93,9 @@ static void Synchronizer_SuspendThread(MutatorThread *thread) {
     if (!ResetEvent(thread->wakeupEvent)) {
         fprintf(stderr, "Failed to reset event %lu\n", GetLastError());
     }
-    while (WaitForSingleObject(thread->wakeupEvent, INFINITE) !=
-           WAIT_OBJECT_0) {
-        thread_yield();
+    if (WAIT_OBJECT_0 != WaitForSingleObject(thread->wakeupEvent, INFINITE)) {
+        fprintf(stderr, "Error: suspend thread");
+        exit(GetLastError());
     }
 #else
     int signum;
@@ -136,7 +135,9 @@ void Synchronizer_wait() {
     atomic_thread_fence(memory_order_seq_cst);
 
     atomic_store_explicit(&self->isWaiting, true, memory_order_release);
-    Synchronizer_SuspendThread(self);
+    while (isCollecting) {
+        Synchronizer_SuspendThread(self);
+    }
     atomic_store_explicit(&self->isWaiting, false, memory_order_release);
 
     MutatorThread_switchState(self, MutatorThreadState_Managed);
@@ -157,6 +158,7 @@ bool Synchronizer_acquire() {
     // Don't allow for registration of any new threads;
     MutatorThreads_lock();
     Safepoint_arm(SafepointInstance);
+    atomic_thread_fence(memory_order_seq_cst);
 
     int iteration = 0;
     int activeThreads;
@@ -179,24 +181,21 @@ bool Synchronizer_acquire() {
 void Synchronizer_release() {
     Safepoint_disarm(SafepointInstance);
     isCollecting = false;
+    atomic_thread_fence(memory_order_seq_cst);
+
     int stoppedThreads;
     do {
-        MutatorThreads_foreach(mutatorThreads, node) {
-            MutatorThread *thread = node->value;
-            if (atomic_load_explicit(&thread->isWaiting,
-                                     memory_order_acquire)) {
-                Synchronizer_WakupThread(thread);
-            }
-        }
-        thread_yield();
         stoppedThreads = 0;
         MutatorThreads_foreach(mutatorThreads, node) {
             MutatorThread *thread = node->value;
             if (atomic_load_explicit(&thread->isWaiting,
                                      memory_order_acquire)) {
                 stoppedThreads++;
+                Synchronizer_WakupThread(thread);
             }
         }
+        if (stoppedThreads > 0)
+            thread_yield();
     } while (stoppedThreads > 0);
     MutatorThread_switchState(currentMutatorThread, MutatorThreadState_Managed);
     MutatorThreads_unlock();

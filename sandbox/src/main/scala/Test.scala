5ee009d8b2c2efa4731d7c90c2fc2b6df92b1342
Wojciech Mazur <wmazur@virtuslab.com>
2024-02-09 00:33:59 +0100

    debug

--- sandbox/src/main/scala/Test.scala
+++ sandbox/src/main/scala/Test.scala
@@ -1,5 +1,12 @@
 object Test {
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
+    val arr: Array[AnyRef] = Array.fill(16388)(new {}) 
+    val t = new Thread(() => {System.gc(); println(arr)} )
+    println(arr.size)
+    t.start()
+    t.join()
+    println(arr)
+
   }
 }

d17c9698bc5600b99bf447bbb268c89b7c989f35
Wojciech Mazur <wmazur@virtuslab.com>
2024-02-09 19:20:35 +0100

    Test case without threads

--- sandbox/src/main/scala/Test.scala
+++ sandbox/src/main/scala/Test.scala
@@ -1,12 +1,25 @@
 object Test {
+  def testRefArrayAlloc(size: Int): Unit = {
+    case class Elem(inner: Elem)
+    val arr = Array.fill(size)(new Elem(new Elem(null)))
+    println("Start GC")
+    System.gc()
+    println("GC done")
+    locally {
+      arr.zipWithIndex.foreach { case (elem, idx) =>
+        assert(
+          elem != null && elem.inner != null && elem.inner.inner == null,
+          s"Invalid: idx=$idx, elem=$elem"
+        )
+      }
+      println(s"checks done $size")
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-    val arr: Array[AnyRef] = Array.fill(16388)(new {}) 
-    val t = new Thread(() => {System.gc(); println(arr)} )
-    println(arr.size)
-    t.start()
-    t.join()
-    println(arr)
-
+    Array(0, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384)
+      .foreach(testRefArrayAlloc)
+    println("done")
   }
 }

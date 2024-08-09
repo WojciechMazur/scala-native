62df434c0071c20d63d17f205c29fdaea0a8954a
Wojciech Mazur <wmazur@virtuslab.com>
2024-08-09 12:10:34 +0200

    Try debug output file

--- test-runner/src/main/scala/scala/scalanative/testinterface/ProcessRunner.scala
+++ test-runner/src/main/scala/scala/scalanative/testinterface/ProcessRunner.scala
@@ -27,6 +27,9 @@ private[testinterface] class ProcessRunner(
     }
     if (emulatorOpts.nonEmpty) {
       logger.info(s"Using test process emulator: ${emulatorOpts.mkString(" ")}")
+      new ProcessBuilder("bash", "-c", "file", executableFile.getAbsolutePath())
+        .inheritIO()
+        .start()
     }
 
     val builder =

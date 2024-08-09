02294fa25fae6e2d3f7008d4836cac161994eeb7
Wojciech Mazur <wmazur@virtuslab.com>
2024-08-09 18:57:45 +0200

    Fix config setting for sysroot - allow on i386

--- test-runner/src/main/scala/scala/scalanative/testinterface/ProcessRunner.scala
+++ test-runner/src/main/scala/scala/scalanative/testinterface/ProcessRunner.scala
@@ -27,7 +27,11 @@ private[testinterface] class ProcessRunner(
     }
     if (emulatorOpts.nonEmpty) {
       logger.info(s"Using test process emulator: ${emulatorOpts.mkString(" ")}")
-      new ProcessBuilder("bash", "-c", s"file ${executableFile.getAbsolutePath()}")
+      new ProcessBuilder(
+        "bash",
+        "-c",
+        s"file ${executableFile.getAbsolutePath()}"
+      )
         .inheritIO()
         .start()
     }

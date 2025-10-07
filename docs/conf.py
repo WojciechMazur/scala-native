432e38db1f964cf4f3586db964c161df161d2844
Wojciech Mazur <wmazur@virtuslab.com>
2025-10-07 19:43:40 +0200

    restore docs setup

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,10 @@ def generateScalaNativeCurrentYear():
     return str(date.today().year).encode("utf-8").decode("utf-8")
 
 def lastSnapshotVersion():
-    # Reconstructs version based on the last-published tag
+    # Ensures we're inside a repo (raises if not)
+    _ensure_repo()
+    # Recosntructs version based on the last-published tag
     tagName="last-published"
-    _git(["fetch", "--tags"])
     date = tag_commit_date_utc(tagName)
     sha7 = tag_commit_sha(tagName)[:7]
     return f"{developed_version}-{date}-{sha7}-SNAPSHOT"
